86450b9050a439acd4c88f4ba3d1cf15031a60fd
tvalentijn <tjerk.valentijn@visma.com>
2021-11-19 15:29:25 +0100

    adding sets on click

--- src/index.js
+++ src/index.js
@@ -1,50 +1,42 @@
-import {findParent, on, setProp, dom, setAttr, hasClass} from "saladbar";
-import {chain, curry, pipe} from "ramda";
-
-// utility function to push out some ugly details
-const stringify = (obj = {}) => JSON.stringify(obj, null, 2);
-
-// TODO make safe like saladbar
-// TODO accept an Eiter(el) too
-const getAttribute = curry((attributeName, el) => el.getAttribute(attributeName));
-// TODO take an Either
-const cloneElement = (el) => el.cloneNode();
-
-const writeToOutput = (data = {}) =>
-  setProp("innerHTML", stringify(data), "#pre-output");
+import { on, setProp, dom, getProp } from "saladbar";
+import { concat, pipe } from "ramda";
+import Either from "data.either";
 
 const logAndPass =
   (msg = "") =>
-    (value) => {
-      console.log(msg, value);
-      return value;
-    };
+  (value) => {
+    console.log(msg, value);
+    return value;
+  };
 
-// note that 'serialize' needs a 'form' element
-// const addSet = pipe(
-//   dom("#fieldset-set"),
-//   logAndPass("post-dom"),
-//   // writeToOutput
-// );
+const makeTemplateEither = (selector) =>
+  dom(selector).chain(getProp("innerHTML"));
+
+const templateFieldsetSetEither = makeTemplateEither("#template-fieldset-set");
 
 /* ------------- side effects for runtime below ---------------- */
+
+// inject first Set in the DOM from a template
+setProp("innerHTML", templateFieldsetSetEither, dom(".cell-fieldset-set"));
 
 on(
   "click",
   (evt) => {
-    // ALGO:
-    // 1. get a specific parent for the clicked button (.grid-row)
-    // 2. get the second .cell in the row and append a child
-    //addSet(); // DOM element to grab data from
-    // const findGridRow = findParent(hasClass(".grid-row"));
-    // findGridRow(dom(".fieldset-set")).map(console.log);
+    console.log("evt", evt.target.value);
 
-    // const parent = findParent(evt.target);
-    // logAndPass("evt")(setAttr("data-joehoe", "66")(dom(".fieldset-set")))
+    const parentEither = dom(
+      `div[data-row-id='${evt.target.value}'] .cell-fieldset-set`
+    );
 
-    // works:
-    // const newEl = pipe(dom, chain(cloneElement))(".fieldset-set");
-    // console.log(newEl);
+    const currentSetsEither = parentEither.chain(getProp("innerHTML"));
+
+    // works
+    pipe(
+      setProp(
+        "innerHTML",
+        Either.of(concat).ap(currentSetsEither).ap(templateFieldsetSetEither)
+      )
+    )(parentEither);
   },
   ".btn-add-set" // DOM element to 'click' on
 );
