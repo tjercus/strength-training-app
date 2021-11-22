35be5b0d045c476d831200072ddf51416b8d09e6
tvalentijn <tjerk.valentijn@visma.com>
2021-11-22 09:32:16 +0100

    addSets and addExercise working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import { on, setProp, dom, getProp } from "saladbar";
-import { concat, pipe } from "ramda";
+import { on, setProp, dom, getProp, setAttr } from "saladbar";
+import { concat, curry, pipe } from "ramda";
 import Either from "data.either";
+import { v4 as uuid } from "uuid";
 
+// debug function to put in a composition
 const logAndPass =
   (msg = "") =>
   (value) => {
@@ -9,34 +11,87 @@ const logAndPass =
     return value;
   };
 
-const makeTemplateEither = (selector) =>
-  dom(selector).chain(getProp("innerHTML"));
+/* -------------- utils for working with the DOM (not in Saladbar) ------------------ */
 
-const templateFieldsetSetEither = makeTemplateEither("#template-fieldset-set");
+/**
+ * Given a selector or Either<Error, DOM Element>, return it's innerHTML
+ * getInnerHtmlEither :: String | Selector -> Either Error String
+ */
+const getInnerHtmlEither = (selOrEl) =>
+  typeof selOrEl === "string"
+    ? dom(selOrEl).chain(getProp("innerHTML"))
+    : selOrEl.chain(getProp("innerHTML"));
 
-/* ------------- side effects for runtime below ---------------- */
+/**
+ * setInnerHtml :: DOM Element -> Either Error DOM Element
+ */
+const setInnerHtml = setProp("innerHTML");
 
-// inject first Set in the DOM from a template
-setProp("innerHTML", templateFieldsetSetEither, dom(".cell-fieldset-set"));
+/**
+ * Given two Eithers, make a new one with both it's contents concatenated (string or list)
+ * Note that if one of the Eithers is a Left (null etc.) then the result will be Left
+ * concatEithers :: (Either Error a, Either Error a) -> Either Error a
+ */
+const concatEithers = (anEither, anotherEither) =>
+  Either.of(concat).ap(anEither).ap(anotherEither);
+
+/**
+ * Takes a default and an Either and returns original or new Either(defaultValue)
+ * withDefault :: a -> Either Error a -> Either Error a
+ */
+const withDefault = curry((defaultValue, ei) =>
+  ei.isRight ? ei : Either.of(defaultValue)
+);
+
+/* --------------------------- application specific utils ------------------------- */
+
+const templateGridRowHtmlEither = getInnerHtmlEither("#template-grid-row");
+const templateCellSetsHtmlEither = getInnerHtmlEither("#template-cell-sets");
+
+/**
+ * Add a new blank workout Set to the cell of Sets
+ * addSetToCell :: string -> Either Error DOM Element
+ */
+const addSetToCell = (rowId) => {
+  const cellSetsEither = dom(`div[data-row-id='${rowId}'] .cell-sets`);
+  const currentHtmlEither = pipe(
+    getInnerHtmlEither,
+    withDefault("")
+  )(cellSetsEither);
+
+  setInnerHtml(
+    concatEithers(currentHtmlEither, templateCellSetsHtmlEither),
+    cellSetsEither
+  );
+};
+
+const addExercise = () => {
+  const gridBodyEither = dom(".grid-body");
+  const currentHtmlEither = pipe(
+    getInnerHtmlEither,
+    withDefault("")
+  )(gridBodyEither);
+
+  setInnerHtml(
+    concatEithers(currentHtmlEither, templateGridRowHtmlEither),
+    gridBodyEither
+  ).map((rows) => {
+    const newId = uuid();
+    setAttr("data-row-id", newId, dom("div.grid-row:last-child", rows));
+    setAttr("value", newId, dom("div.grid-row:last-child .btn-add-set", rows));
+    addSetToCell(newId);
+  });
+};
+
+/* ---------------------- side effects for runtime below ------------------------ */
+
+// inject first Exercise (with one Set) in the DOM (from a template)
+addExercise();
 
 on(
   "click",
-  (evt) => {
-    console.log("evt", evt.target.value);
-
-    const parentEither = dom(
-      `div[data-row-id='${evt.target.value}'] .cell-fieldset-set`
-    );
-
-    const currentSetsEither = parentEither.chain(getProp("innerHTML"));
-
-    // works
-    pipe(
-      setProp(
-        "innerHTML",
-        Either.of(concat).ap(currentSetsEither).ap(templateFieldsetSetEither)
-      )
-    )(parentEither);
-  },
-  ".btn-add-set" // DOM element to 'click' on
+  (evt) => addSetToCell(evt.target.value), // TODO filter to handle ONLY .btn-add-set
+  ".grid" // click on a parent of all buttons to use event-bubbling
 );
+
+on("click", () => addExercise(), "#btn-add-exercise");
